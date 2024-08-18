72011b012525424bd0ee933d457d55ad4f70ba30
Joe Bowbeer <joe.bowbeer@gmail.com>
2024-08-18 02:44:20 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::{
+    cmp::min,
     collections::{hash_map::Entry, HashMap},
     io::{stdin, BufRead, BufReader, Error},
+    thread,
 };
 
 /// Reads an ordered list of words from stdin and prints the determined
 /// character sort order
 fn main() {
     let words = read_words(stdin().lock()).expect("read words");
-    println!("{:?}", words);
-    let (slices, chars) = index_chars(&words);
-    for slice in slices {
-        println!("{:?}", String::from_iter(restore_chars(&slice, &chars)));
-    }
+    // println!("{:?}", words);
+    println!("{:?}", lexical_order(&words).expect("sorted"));
 }
 
 // Returns words read from input
@@ -22,23 +21,54 @@ fn read_words<R: BufRead>(input: R) -> Result<Vec<String>, Error> {
 
 /// Receives a list of words that are sorted according to an unknown
 /// character precedence and returns their characters in the determined order
-fn _lexical_order(words: &[String]) -> Vec<char> {
-    let (slices, chars) = index_chars(words);
+fn lexical_order(words: &[String]) -> Result<Vec<char>, String> {
+    let (indexed, chars) = index_chars(words);
     let dim = chars.len();
-    let mut dist = _adjacency(slices, dim);
+    let mut dist = adjacency(&indexed, dim);
     let mut pow = 1;
     while pow < dim {
-        dist = _maxplus(dist);
+        dist = maxplus(&dist);
         pow <<= 1;
     }
-    restore_chars(&_sorted_indices(dist), &chars)
+    Ok(restore_chars(&sorted_indices(&dist)?, &chars))
+}
+
+#[test]
+fn lexical_order_test() {
+    assert_eq!(
+        Ok(vec![]),
+        lexical_order(&[].map(|s: &str| s.to_string())),
+        "(empty)"
+    );
+    assert_eq!(
+        Ok(vec!['a']),
+        lexical_order(&["a", "aa"].map(|s: &str| s.to_string())),
+        "a"
+    );
+    assert_eq!(
+        Ok(vec!['a', 'b']),
+        lexical_order(&["a", "b"].map(|s: &str| s.to_string())),
+        "ab"
+    );
+    // Test cases from:
+    // https://www.geeksforgeeks.org/given-sorted-dictionary-find-precedence-characters/
+    assert_eq!(
+        Ok(vec!['b', 'a', 'c']),
+        lexical_order(&["bca", "aaa", "acb"].map(|s| s.to_string())),
+        "bac"
+    );
+    assert_eq!(
+        Ok(vec!['b', 'd', 'a', 'c']),
+        lexical_order(&["baa", "abcd", "abca", "cab", "cad"].map(|s| s.to_string())),
+        "bdac"
+    );
 }
 
 /// Given a list of words, converts each char to an index into a list of chars.
 /// Returns the indices and the indexed list of chars.
 fn index_chars(words: &[String]) -> (Vec<Vec<usize>>, Vec<char>) {
     let mut char_map = HashMap::new();
-    let mut slices = Vec::with_capacity(words.len());
+    let mut indexed = Vec::with_capacity(words.len());
     let mut chars = Vec::new();
     for word in words {
         let word_chars = word.chars().collect::<Vec<_>>();
@@ -57,9 +87,9 @@ fn index_chars(words: &[String]) -> (Vec<Vec<usize>>, Vec<char>) {
             }
             indices.push(index);
         }
-        slices.push(indices);
+        indexed.push(indices);
     }
-    (slices, chars)
+    (indexed, chars)
 }
 
 /// Given a list of indices and an indexed list of chars,
@@ -68,99 +98,92 @@ fn restore_chars(indices: &[usize], chars: &[char]) -> Vec<char> {
     indices.iter().map(|index| chars[*index]).collect()
 }
 
-/// Returns adjacency matrix given sorted list of slices containing
+/// Returns adjacency matrix given sorted list of sequences containing
 /// indices in range [0..dim)
-fn _adjacency(_input: Vec<Vec<usize>>, _dim: usize) -> Vec<Vec<usize>> {
-    // // initialize output
-    // let output = make([][]int, dim)
-    // for i := range out {
-    // 	out[i] = make([]int, dim)
-    // }
-    // for i := 1; i < len(in); i++ {
-    // 	// pred precedes succ in lexical order
-    // 	pred := in[i-1]
-    // 	succ := in[i]
-    // 	// find first position where their ints differ
-    // 	for k := 0; k < len(pred) && k < len(succ); k++ {
-    // 		if pred[k] != succ[k] {
-    // 			out[pred[k]][succ[k]] = 1
-    // 			break
-    // 		}
-    // 	}
-    // }
-    Vec::new()
+fn adjacency(indexed: &[Vec<usize>], dim: usize) -> Vec<Vec<usize>> {
+    // initialize output
+    let mut adj = vec![vec![0; dim]; dim];
+    for i in 1..indexed.len() {
+        // pred precedes succ in lexical order
+        let pred = &indexed[i - 1];
+        let succ = &indexed[i];
+        // find first position where their ints differ
+        for k in 0..min(pred.len(), succ.len()) {
+            if pred[k] != succ[k] {
+                adj[pred[k]][succ[k]] = 1;
+                break;
+            }
+        }
+    }
+    adj
 }
 
 /// Returns max-plus product of the given distance matrix
-fn _maxplus(_input: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
-    // dim := len(in)
-    // out := make([][]int, dim)
+fn maxplus(dist: &[Vec<usize>]) -> Vec<Vec<usize>> {
+    let dim = dist.len();
+    let mut prod = vec![vec![0; dim]; dim];
+    let num_chunks = thread::available_parallelism().unwrap().get();
+    let chunk_size = (dim + num_chunks - 1) / num_chunks;
     // var wg sync.WaitGroup
-    // chunkSize := (dim + numChunks - 1) / numChunks
-    // for i := 0; i < dim; i += chunkSize {
-    // 	end := i + chunkSize
-    // 	if end > dim {
-    // 		end = dim
-    // 	}
-    // 	wg.Add(1)
-    // 	go func(m, n int) {
-    // 		defer wg.Done()
-    // 		for i := m; i < n; i++ {
-    // 			out[i] = maxplusRow(in, i)
-    // 		}
-    // 	}(i, end)
-    // }
+    for i in (0..dim).step_by(chunk_size) {
+        let end = min(i + chunk_size, dim);
+        // wg.Add(1)
+        (|m, n| {
+            for i in m..n {
+                prod[i] = maxplus_row(dist, i);
+            }
+            // wg.Done()
+        })(i, end)
+    }
     // wg.Wait()
-    Vec::new()
+    prod
 }
 
 /// Returns one row of the max-plus product of the given distance matrix
-fn _maxplus_row(_input: Vec<Vec<usize>>, _i: usize) -> Vec<usize> {
-    // dim := len(in)
-    // row := make([]int, dim)
-    // for j := 0; j < dim; j++ {
-    // 	if i == j {
-    // 		continue
-    // 	}
-    // 	max := in[i][j]
-    // 	for k := 0; k < dim; k++ {
-    // 		if in[i][k] == 0 || in[k][j] == 0 {
-    // 			continue
-    // 		}
-    // 		if sum := in[i][k] + in[k][j]; sum > max {
-    // 			max = sum
-    // 		}
-    // 	}
-    // 	row[j] = max
-    // }
-    Vec::new()
+fn maxplus_row(dist: &[Vec<usize>], i: usize) -> Vec<usize> {
+    let dim = dist.len();
+    let mut row = vec![0; dim];
+    for j in 0..dim {
+        if i == j {
+            continue;
+        }
+        let mut max = dist[i][j];
+        for k in 0..dim {
+            if dist[i][k] == 0 || dist[k][j] == 0 {
+                continue;
+            }
+            let sum = dist[i][k] + dist[k][j];
+            if sum > max {
+                max = sum
+            }
+        }
+        row[j] = max
+    }
+    row
 }
 
+// Index usize::MAX is a reserved to designate an unranked element
+const UNRANKED: usize = usize::MAX;
+
 /// Returns row indices ordered by the longest distance found in each row
-fn _sorted_indices(_dist: Vec<Vec<usize>>) -> Vec<usize> {
-    // dim := len(dist)
-    // out := make([]int, dim)
-    // for i := range out {
-    // 	out[i] = -1
-    // }
-    // for index := range dist {
-    // 	// find maximum distance in row
-    // 	max := 0
-    // 	for _, d := range dist[index] {
-    // 		if d > max {
-    // 			max = d
-    // 		}
-    // 	}
-    // 	if max >= dim {
-    // 		log.Fatalf("Fail! Cycle detected at index %d", index)
-    // 	}
-    // 	rank := dim - max - 1
-    // 	if out[rank] != -1 {
-    // 		log.Fatalf(
-    // 			"Fail! Rank %d of index %d already assigned to index %d",
-    // 			rank, index, out[rank])
-    // 	}
-    // 	out[rank] = index
-    // }
-    Vec::new()
+fn sorted_indices(dist: &[Vec<usize>]) -> Result<Vec<usize>, String> {
+    let dim = dist.len();
+    assert!(dim < UNRANKED);
+    let mut indices = vec![UNRANKED; dim];
+    for (index, row) in dist.iter().enumerate() {
+        // find maximum distance in row
+        let max = *row.iter().max().unwrap();
+        if max >= dim {
+            return Err(format!("Fail! Cycle detected at index {index}"));
+        }
+        let rank = dim - max - 1;
+        if indices[rank] != UNRANKED {
+            return Err(format!(
+                "Fail! Rank {rank} of index {index} already assigned to index {}",
+                indices[rank]
+            ));
+        }
+        indices[rank] = index;
+    }
+    Ok(indices)
 }
